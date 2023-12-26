ce1fa95808a4297968d0cd8f30220bbed94ae199
Milad Karandish <80484107+MiladKarandish@users.noreply.github.com>
2023-12-26 21:40:41 +0330

    trying to convert to pdf

--- src/templates/main/index.tsx
+++ src/templates/main/index.tsx
@@ -1,7 +1,46 @@
+import styles from "./main-template.module.scss";
+
 export interface Props {}
 
 const MainTemplate = () => {
-  return <div>New Fucking Component</div>;
+  interface IContactLinks {
+    title: string;
+    href: string;
+  }
+  const contactLinks: IContactLinks[] = [
+    {
+      title: "miladkaarandish@gmail.com",
+      href: "https://miladkaarandish@gmail.com",
+    },
+  ];
+
+  return (
+    <div className={`${styles["_"]}`}>
+      <main>
+        <header>
+          <h1 className={`${styles["_name"]}`}>Milad Karandish</h1>
+          <h3 className={`${styles["_field"]}`}>Front End Developer</h3>
+        </header>
+
+        {/* Experient */}
+        <section></section>
+
+        {/* Projects */}
+      </main>
+
+      {/* Side */}
+      <aside>
+        {/* Links */}
+        <div>
+          {contactLinks.map((contact) => (
+            <a href={contact.href} target="_blank" key={contact.title}>
+              {contact.title}
+            </a>
+          ))}
+        </div>
+      </aside>
+    </div>
+  );
 };
 
 export default MainTemplate;
