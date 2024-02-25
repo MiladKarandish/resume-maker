eef7c9abae7286ea91fd539c191f8354c20f3e92
Milad Karandish <80484107+MiladKarandish@users.noreply.github.com>
2024-02-25 16:13:28 +0330

    Description changed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,8 +6,10 @@ const Header = () => {
       <h1 className={`${styles["_name"]}`}>Milad Karandish</h1>
       <h3 className={`${styles["_field"]}`}>Front End Developer</h3>
 
+      {/* front-end web developer with nearly 4 years of experience. */}
       <p className={`${styles["_about-me"]}`}>
-        front-end web developer with nearly 4 years of experience.
+        Specializing in React, Next.js, Vue, Nuxt, and TypeScript. Dedicated to
+        crafting intuitive user interfaces for enhanced user experience
       </p>
     </>
   );
