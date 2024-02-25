71990608fd7e8437caf6e2ff05df78579ca4a14a
Milad Karandish <80484107+MiladKarandish@users.noreply.github.com>
2024-02-25 16:41:05 +0330

    trying to find the best description

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,10 +8,10 @@ const Header = () => {
 
       {/* front-end web developer with nearly 4 years of experience. */}
       <p className={`${styles["_about-me"]}`}>
-        Skilled Frontend Developer proficient in JavaScript, React, and Next.js.
-        Experienced in Redux, Node.js, and CSS frameworks like Sass and
-        Bootstrap. Knowledgeable in GraphQL, REST APIs, and MongoDB. Strong
-        focus on UX/UI optimization and SEO best practices.
+        Frontend Developer skilled in JavaScript, React, and Next.js, with
+        experience in Redux, Node.js, and CSS frameworks. Proficient in GraphQL,
+        REST APIs, and MongoDB. Focused on UX/UI optimization and SEO best
+        practices.
       </p>
     </>
   );
